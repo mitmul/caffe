080875f4dae4f2c518ff87c3af51b3687709d6c0
Shunta Saito <shunta.saito@gmail.com>
2014-10-06 11:48:06 +0900

    use some variables

--- src/caffe/layers/labeling_loss_layer.cu
+++ src/caffe/layers/labeling_loss_layer.cu
@@ -11,8 +11,9 @@
 namespace caffe {
 
 template <typename Dtype>
-__global__ void kernel_loss(const int num, const int spatial_dim, const int dim,
-                            const Dtype *label, const Dtype *prob, Dtype *out) {
+__global__ void kernel_loss(
+  const int num, const int spatial_dim, const int dim,
+  const Dtype *label, const Dtype *prob, Dtype *out) {
   CUDA_KERNEL_LOOP(index, num * spatial_dim) {
     int i = index / spatial_dim;
     int j = index % spatial_dim;
@@ -23,8 +24,9 @@ __global__ void kernel_loss(const int num, const int spatial_dim, const int dim,
 }
 
 template <typename Dtype>
-__global__ void kernel_diff(const int num, const int spatial_dim, const int dim,
-                            const Dtype *label, Dtype *out) {
+__global__ void kernel_diff(
+  const int num, const int spatial_dim, const int dim,
+  const Dtype *label, Dtype *out) {
   CUDA_KERNEL_LOOP(index, num * spatial_dim) {
     int i = index / spatial_dim;
     int j = index % spatial_dim;
@@ -39,19 +41,18 @@ void LabelingLossLayer<Dtype>::Forward_gpu(
   const vector<Blob<Dtype>*> &top) {
   // The forward pass computes the softmax prob values.
   softmax_layer_->Forward(softmax_bottom_vec_, softmax_top_vec_);
+  const Dtype *bottom_data = bottom[1]->gpu_data();
   const Dtype *prob_data = prob_.gpu_data();
-  const Dtype *bottom_label = bottom[1]->gpu_data();
+  Dtype *loss_data = loss_.mutable_gpu_data();
   const int num = prob_.num();
   const int dim = prob_.count() / num;
   const int spatial_dim = prob_.height() * prob_.width();
-  Dtype *loss_vec = loss_.mutable_gpu_data();
 
   // NOLINT_NEXT_LINE(whitespace/operators)
-  kernel_loss<Dtype> <<< CAFFE_GET_BLOCKS(num *spatial_dim),
-              CAFFE_CUDA_NUM_THREADS>>>(num, spatial_dim, dim,
-                                        bottom_label, prob_data, loss_vec);
-  Dtype loss;
-  caffe_gpu_asum<Dtype>(num * spatial_dim, loss_vec, &loss);
+  kernel_loss<Dtype>
+  <<< CAFFE_GET_BLOCKS(num *spatial_dim), CAFFE_CUDA_NUM_THREADS>>>
+  (num, spatial_dim, dim, bottom_data, prob_data, loss_data);
+  Dtype loss = loss_.asum_data();
   top[0]->mutable_cpu_data()[0] = loss / num / spatial_dim;
 }
 
@@ -66,17 +67,17 @@ void LabelingLossLayer<Dtype>::Backward_gpu(
   }
   if (propagate_down[0]) {
     Dtype *bottom_diff = bottom[0]->mutable_gpu_diff();
-    const Dtype *prob_data = prob_.gpu_data();
-    caffe_copy(prob_.count(), prob_data, bottom_diff);
+    caffe_copy(prob_.count(), prob_.gpu_data(), bottom_diff);
     const Dtype *bottom_label = bottom[1]->gpu_data();
     int num = prob_.num();
     int dim = prob_.count() / num;
     int spatial_dim = prob_.height() * prob_.width();
 
     // NOLINT_NEXT_LINE(whitespace/operators)
-    kernel_diff<Dtype> <<< CAFFE_GET_BLOCKS(num *spatial_dim),
-                CAFFE_CUDA_NUM_THREADS>>>(num, spatial_dim, dim,
-                                          bottom_label, bottom_diff);
+    kernel_diff<Dtype>
+    <<< CAFFE_GET_BLOCKS(num *spatial_dim), CAFFE_CUDA_NUM_THREADS>>>
+    (num, spatial_dim, dim, bottom_label, bottom_diff);
+
     // Scale gradient
     const Dtype loss_weight = top[0]->cpu_diff()[0];
     caffe_gpu_scal<Dtype>(prob_.count(), loss_weight / num / spatial_dim, bottom_diff);
