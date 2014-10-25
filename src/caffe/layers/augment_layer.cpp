5a6d706f044eae0a75d0a5e6f0233e5473769058
Shunta Saito <shunta.saito@gmail.com>
2014-10-26 02:21:12 +0900

    add augment layer test

--- src/caffe/layers/augment_layer.cpp
+++ src/caffe/layers/augment_layer.cpp
@@ -7,8 +7,15 @@ namespace caffe {
 template <typename Dtype>
 void AugmentLayer<Dtype>::Reshape(const vector<Blob<Dtype>*> &bottom,
                                   const vector<Blob<Dtype>*> &top) {
-  top[0]->ReshapeLike(*bottom[0]);
-  top[1]->ReshapeLike(*bottom[1]);
+  const int data_size =
+    this->layer_param_.augment_param().data_crop_size();
+  const int label_size =
+    this->layer_param_.augment_param().label_crop_size();
+
+  top[0]->Reshape(bottom[0]->num(), bottom[0]->channels(),
+                  data_size, data_size);
+  top[1]->Reshape(bottom[1]->num(), bottom[1]->channels(),
+                  label_size, label_size);
 }
 
 template <typename Dtype>
@@ -35,12 +42,14 @@ void AugmentLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*> &bottom,
     // randomly rotate
     if (this->layer_param_.augment_param().rotate()) {
       const double angle = static_cast<double>(caffe_rng_rand() % 360);
-      cv::Point2f pt(data_height / 2.0, data_width / 2.0);
-      cv::Mat rot = cv::getRotationMatrix2D(pt, angle, 1.0);
-      cv::warpAffine(data_img, data_img, rot,
-                     cv::Size(data_height, data_width));
-      cv::warpAffine(label_img, label_img, rot,
-                     cv::Size(label_height, label_width));
+      cv::Point2f data_pt(data_width / 2.0, data_height / 2.0);
+      cv::Mat data_rot = cv::getRotationMatrix2D(data_pt, angle, 1.0);
+      cv::warpAffine(data_img, data_img, data_rot,
+                     cv::Size(data_width, data_height));
+      cv::Point2f label_pt(label_width / 2.0, label_height / 2.0);
+      cv::Mat label_rot = cv::getRotationMatrix2D(label_pt, angle, 1.0);
+      cv::warpAffine(label_img, label_img, label_rot,
+                     cv::Size(label_width, label_height));
     }
 
     // crop center
@@ -48,15 +57,14 @@ void AugmentLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*> &bottom,
       this->layer_param_.augment_param().data_crop_size();
     const int label_size =
       this->layer_param_.augment_param().label_crop_size();
-    LOG(INFO) << data_width / 2 - data_size / 2 << " , "
-              << data_height / 2 - data_size / 2 << " , "
-              << data_size << " , " << data_size;
-    cv::Mat data_patch = data_img(cv::Rect(data_width / 2 - data_size / 2,
-                                           data_height / 2 - data_size / 2,
-                                           data_size, data_size)).clone();
-    cv::Mat label_patch = label_img(cv::Rect(label_width / 2 - label_size / 2,
-                                    label_height / 2 - label_size / 2,
-                                    label_size, label_size)).clone();
+    cv::Mat data_patch(data_size, data_size, CV_64FC(data_channels));
+    data_img(cv::Rect(data_width / 2 - data_size / 2,
+                      data_height / 2 - data_size / 2,
+                      data_size, data_size)).copyTo(data_patch);
+    cv::Mat label_patch(label_size, label_size, CV_64FC(label_channels));
+    label_img(cv::Rect(label_width / 2 - label_size / 2,
+                       label_height / 2 - label_size / 2,
+                       label_size, label_size)).copyTo(label_patch);
 
     // mean subtraction
     const google::protobuf::RepeatedField<float> mean =
@@ -66,8 +74,7 @@ void AugmentLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*> &bottom,
       vector<cv::Mat> splitted;
       cv::split(data_patch, splitted);
       for (int j = 0; j < splitted.size(); ++j) {
-        splitted.at(j).convertTo(splitted.at(j), CV_64FC(data_channels),
-                                 1.0, -mean.Get(j));
+        splitted.at(j).convertTo(splitted.at(j), CV_64F, 1.0, -mean.Get(j));
       }
       cv::merge(splitted, data_patch);
     }
@@ -80,8 +87,7 @@ void AugmentLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*> &bottom,
       vector<cv::Mat> splitted;
       cv::split(data_patch, splitted);
       for (int j = 0; j < splitted.size(); ++j) {
-        splitted.at(j).convertTo(splitted.at(j), CV_64FC(data_channels),
-                                 1.0 / stddev.Get(j));
+        splitted.at(j).convertTo(splitted.at(j), CV_64F, 1.0 / stddev.Get(j));
       }
       cv::merge(splitted, data_patch);
     }
@@ -103,8 +109,8 @@ cv::Mat AugmentLayer<Dtype>::ConvertToCVMat(
     for (int h = 0; h < height; ++h) {
       for (int w = 0; w < width; ++w) {
         int index = c * height * width + h * width + w;
-        int pos = h * width * channels + w * channels + c;
         double val = static_cast<double>(data[index]);
+        int pos = h * width * channels + w * channels + c;
         reinterpret_cast<double *>(img.data)[pos] = val;
       }
     }
@@ -117,20 +123,18 @@ template <typename Dtype>
 void AugmentLayer<Dtype>::ConvertFromCVMat(const cv::Mat img, Dtype *data) {
   const int channels = img.channels();
   const int height = img.rows;
-  const int width = img.rows;
+  const int width = img.cols;
   for (int c = 0; c < channels; ++c) {
     for (int h = 0; h < height; ++h) {
       for (int w = 0; w < width; ++w) {
-        int index = c * height * width + h * width + w;
-        float val = reinterpret_cast<double *>(img.data)[
-                      h * width * channels + w * channels + c];
+        const int pos = h * width * channels + w * channels + c;
+        double val = reinterpret_cast<double *>(img.data)[pos];
+        const int index = c * height * width + h * width + w;
         data[index] = static_cast<Dtype>(val);
       }
     }
   }
 }
-
-
 
 INSTANTIATE_CLASS(AugmentLayer);
 REGISTER_LAYER_CLASS(AUGMENT, AugmentLayer);
--- src/caffe/test/test_augment_layer.cpp
+++ src/caffe/test/test_augment_layer.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <opencv2/opencv.hpp>
 
 #include "gtest/gtest.h"
 
@@ -21,7 +22,9 @@ class AugmentLayerTest : public MultiDeviceTest<TypeParam> {
 
  protected:
   AugmentLayerTest()
-    : blob_top_data_(new Blob<Dtype>()),
+    : blob_bottom_data_(new Blob<Dtype>()),
+      blob_bottom_label_(new Blob<Dtype>()),
+      blob_top_data_(new Blob<Dtype>()),
       blob_top_label_(new Blob<Dtype>()) {}
 
   virtual void SetUp() {
@@ -34,17 +37,19 @@ class AugmentLayerTest : public MultiDeviceTest<TypeParam> {
 
     LayerParameter data_param;
     DataParameter *data_dparam = data_param.mutable_data_param();
-    data_dparam->set_batch_size(5);
+    data_dparam->set_batch_size(10);
     data_dparam->set_source(data_db_file.c_str());
     data_dparam->set_backend(DataParameter_DB_LMDB);
     data_layer_ = new DataLayer<Dtype>(data_param);
-
     vector<Blob<Dtype>*> blob_data_layer_vec;
-    blob_data_layer_vec.push_back(blob_top_data_);
+    blob_data_layer_vec.push_back(blob_bottom_data_);
     data_layer_->SetUp(blob_bottom_vec_, blob_data_layer_vec);
     data_layer_->Forward(blob_bottom_vec_, blob_data_layer_vec);
 
-    LOG(INFO) << "data num: " << blob_top_data_->num();
+    LOG(INFO) << "data num: " << blob_bottom_data_->num();
+    LOG(INFO) << "data channels: " << blob_bottom_data_->channels();
+    LOG(INFO) << "data height: " << blob_bottom_data_->height();
+    LOG(INFO) << "data width: " << blob_bottom_data_->width();
 
     // load label
     string label_db_file =
@@ -52,29 +57,71 @@ class AugmentLayerTest : public MultiDeviceTest<TypeParam> {
 
     LayerParameter label_param;
     DataParameter *label_dparam = label_param.mutable_data_param();
-    label_dparam->set_batch_size(5);
+    label_dparam->set_batch_size(10);
     label_dparam->set_source(label_db_file.c_str());
     label_dparam->set_backend(DataParameter_DB_LMDB);
     label_layer_ = new DataLayer<Dtype>(label_param);
-
     vector<Blob<Dtype>*> blob_label_layer_vec;
-    blob_label_layer_vec.push_back(blob_top_label_);
+    blob_label_layer_vec.push_back(blob_bottom_label_);
     label_layer_->SetUp(blob_bottom_vec_, blob_label_layer_vec);
     label_layer_->Forward(blob_bottom_vec_, blob_label_layer_vec);
 
-    LOG(INFO) << "label num: " << blob_top_label_->num();
+    LOG(INFO) << "label num: " << blob_bottom_label_->num();
+    LOG(INFO) << "label channels: " << blob_bottom_label_->channels();
+    LOG(INFO) << "label height: " << blob_bottom_label_->height();
+    LOG(INFO) << "label width: " << blob_bottom_label_->width();
 
-    blob_bottom_vec_.push_back(blob_top_data_);
-    blob_bottom_vec_.push_back(blob_top_label_);
+    blob_bottom_vec_.push_back(blob_bottom_data_);
+    blob_bottom_vec_.push_back(blob_bottom_label_);
+
+    string out_dir = CMAKE_SOURCE_DIR "caffe/test/test_data" CMAKE_EXT;
+    SaveData(*blob_bottom_data_, out_dir, "setup_data");
+    SaveData(*blob_bottom_label_, out_dir, "setup_label");
+  }
+
+  void SaveData(const Blob<Dtype> &blob, const string &out_dir,
+                const string &prefix) {
+    // save data
+    const int num = blob.num();
+    const int channels = blob.channels();
+    const int height = blob.height();
+    const int width = blob.width();
+    LOG(INFO) << "SaveData: " << num << " x " << channels
+              << " x " << height << " x " << width;
+    for (int i = 0; i < num; ++i) {
+      const Dtype *data = blob.cpu_data() + blob.offset(i);
+      cv::Mat img(height, width, CV_32FC(channels));
+      for (int c = 0; c < channels; ++c) {
+        for (int y = 0; y < height; ++y) {
+          for (int x = 0; x < width; ++x) {
+            const Dtype pix = data[c * height * width + y * width + x];
+            const int pos = y * width * channels + x * channels + c;
+            reinterpret_cast<float *>(img.data)[pos] = static_cast<float>(pix);
+          }
+        }
+      }
+      cv::Mat dst;
+      cv::normalize(img, dst, 0, 255, cv::NORM_MINMAX);
+      dst.convertTo(dst, CV_8U);
+
+      stringstream ss;
+      ss << out_dir << "/test_data_" << prefix << "_" << i << "_"
+         << channels << "_" << height << "_" << width << ".png";
+      cv::imwrite(ss.str(), dst);
+    }
   }
 
   virtual ~AugmentLayerTest() {
+    delete blob_bottom_data_;
+    delete blob_bottom_label_;
     delete blob_top_data_;
     delete blob_top_label_;
     delete data_layer_;
     delete label_layer_;
   }
 
+  Blob<Dtype> *const blob_bottom_data_;
+  Blob<Dtype> *const blob_bottom_label_;
   Blob<Dtype> *const blob_top_data_;
   Blob<Dtype> *const blob_top_label_;
   vector<Blob<Dtype>*> blob_bottom_vec_;
@@ -90,22 +137,27 @@ TYPED_TEST(AugmentLayerTest, TestRead) {
   LayerParameter param;
   AugmentParameter *augment_param = param.mutable_augment_param();
   augment_param->set_data_crop_size(64);
-  augment_param->set_label_crop_size(64);
+  augment_param->set_label_crop_size(16);
   augment_param->set_rotate(true);
   google::protobuf::RepeatedField<float> *mean =
     augment_param->mutable_mean();
-  mean->Add(77.196875000000006);
-  mean->Add(87.893381076388891);
-  mean->Add(85.800737847222223);
+  mean->Add(75.871057942708333);
+  mean->Add(84.302632378472225);
+  mean->Add(82.718964843750001);
   google::protobuf::RepeatedField<float> *stddev =
     augment_param->mutable_stddev();
-  stddev->Add(41.398123906821638);
-  stddev->Add(39.564715010611259);
-  stddev->Add(41.093476599359363);
+  stddev->Add(50.142589887293951);
+  stddev->Add(48.17382927556428);
+  stddev->Add(50.290859541028532);
+
+  // save image dir
+  string out_dir = CMAKE_SOURCE_DIR "caffe/test/test_data" CMAKE_EXT;
 
   AugmentLayer<Dtype> augment(param);
   augment.SetUp(this->blob_bottom_vec_, this->blob_top_vec_);
   augment.Forward(this->blob_bottom_vec_, this->blob_top_vec_);
+  this->SaveData(*this->blob_top_data_, out_dir, "data");
+  this->SaveData(*this->blob_top_label_, out_dir, "label");
 }
 
 }  // namespace caffe
