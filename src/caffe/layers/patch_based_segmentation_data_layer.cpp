13190cea401ae399fe11d105b793d26a4562113c
Shunta Saito <shunta.saito@gmail.com>
2015-03-24 21:39:10 +0900

    ignore white patches

--- src/caffe/layers/patch_based_segmentation_data_layer.cpp
+++ src/caffe/layers/patch_based_segmentation_data_layer.cpp
@@ -99,6 +99,8 @@ void PatchBasedSegmentationDataLayer<Dtype>::InternalThreadEntry() {
     this->layer_param_.patch_based_segmentation_data_param().rotation();
   const bool flip =
     this->layer_param_.patch_based_segmentation_data_param().flip();
+  const bool has_value =
+    this->layer_param_.patch_based_segmentation_data_param().has_value();
 
   // output of this data layer
   Dtype* top_data = this->prefetch_data_.mutable_cpu_data();
@@ -118,65 +120,82 @@ void PatchBasedSegmentationDataLayer<Dtype>::InternalThreadEntry() {
     const string data_fname = img_fnames[0].get<string>();
     const string label_fname = img_fnames[1].get<string>();
 
-    cv::Mat data = cv::imread(data_fname);
-    cv::Mat label = cv::imread(label_fname, CV_LOAD_IMAGE_GRAYSCALE);
+    cv::Mat _data = cv::imread(data_fname);
+    cv::Mat _label = cv::imread(label_fname, CV_LOAD_IMAGE_GRAYSCALE);
 
-    // cropping left-top point
-    int _x = caffe_rng_rand() % data.cols;
-    const int x = _x + data_width < data.cols ? _x : data.cols - data_width;
-    int _y = caffe_rng_rand() % data.rows;
-    const int y = _y + data_height < data.rows ? _y : data.rows - data_height;
+    while (1) {
+      cv::Mat data = _data.clone();
+      cv::Mat label = _label.clone();
 
-    // rotation
-    if (rotation) {
-      const double angle = caffe_rng_rand() % 360 - 180;
-      const cv::Point2f center(x + data_width / 2, y + data_height / 2);
-      const cv::Mat rot = cv::getRotationMatrix2D(center, angle, 1.0);
-      const cv::Scalar constant(127, 127, 127);
-      cv::warpAffine(data, data, rot, cv::Size(data.cols, data.rows),
-                     cv::INTER_NEAREST, cv::BORDER_CONSTANT, constant);
-      cv::warpAffine(label, label, rot, cv::Size(label.cols, label.rows),
-                     cv::INTER_NEAREST, cv::BORDER_CONSTANT, cv::Scalar(0));
-    }
+      // cropping left-top point
+      int _x = caffe_rng_rand() % data.cols;
+      const int x = _x + data_width < data.cols ? _x : data.cols - data_width;
+      int _y = caffe_rng_rand() % data.rows;
+      const int y = _y + data_height < data.rows ? _y : data.rows - data_height;
 
-    // create patch
-    cv::Mat crop_data = data(cv::Rect(x, y, data_width, data_height));
-    crop_data.convertTo(crop_data, CV_32F);
-    cv::Mat crop_label =
-      label(cv::Rect(x + data_width / 2 - label_width / 2,
-                     y + data_height / 2 - label_height / 2,
-                     label_width, label_height));
-    crop_label.convertTo(crop_label, CV_32F);
+      // rotation
+      if (rotation) {
+        const double angle = caffe_rng_rand() % 360 - 180;
+        const cv::Point2f center(x + data_width / 2, y + data_height / 2);
+        const cv::Mat rot = cv::getRotationMatrix2D(center, angle, 1.0);
+        const cv::Scalar constant(127, 127, 127);
+        cv::warpAffine(data, data, rot, cv::Size(data.cols, data.rows),
+                       cv::INTER_NEAREST, cv::BORDER_CONSTANT, constant);
+        cv::warpAffine(label, label, rot, cv::Size(label.cols, label.rows),
+                       cv::INTER_NEAREST, cv::BORDER_CONSTANT, cv::Scalar(0));
+      }
 
-    // flip
-    const int flip_code = caffe_rng_rand() % 2;
-    if (flip && flip_code == 1) {
-      cv::flip(crop_data, crop_data, flip_code);
-      cv::flip(crop_label, crop_label, flip_code);
-    }
+      // create patch
+      cv::Mat crop_data = data(cv::Rect(x, y, data_width, data_height));
+      crop_data.convertTo(crop_data, CV_32F);
+      cv::Mat crop_label =
+        label(cv::Rect(x + data_width / 2 - label_width / 2,
+                       y + data_height / 2 - label_height / 2,
+                       label_width, label_height));
+      crop_label.convertTo(crop_label, CV_32F);
 
-    // to multi channel label
-    cv::Mat multi_ch_label(
-      label_height, label_width, CV_32FC(label_channels));
-    multi_ch_label =
-      cv::Mat::zeros(label_height, label_width, CV_32FC(label_channels));
-    for (int y = 0; y < label_height; ++y) {
-      for (int x = 0; x < label_width; ++x) {
-        for (int ch = 0; ch < label_channels; ++ch) {
-          if (crop_label.at<float>(y, x) == ch) {
-            int index = y * label_width * label_channels
-                        + x * label_channels + ch;
-            reinterpret_cast<float *>(multi_ch_label.data)[index] = 1.0;
+      // skip too white patch
+      cv::Scalar data_sum = cv::sum(crop_data);
+      if (data_sum[0] > data_width * data_height * 255 * 0.6 &&
+          data_sum[1] > data_width * data_height * 255 * 0.6 &&
+          data_sum[2] > data_width * data_height * 255 * 0.6)
+        continue;
+
+      // must have at least 1 building or road label
+      if (cv::sum(crop_label)[0] == 0 && has_value)
+        continue;
+
+      // flip
+      const int flip_code = caffe_rng_rand() % 2;
+      if (flip && flip_code == 1) {
+        cv::flip(crop_data, crop_data, flip_code);
+        cv::flip(crop_label, crop_label, flip_code);
+      }
+
+      // to multi channel label
+      cv::Mat multi_ch_label(
+        label_height, label_width, CV_32FC(label_channels));
+      multi_ch_label =
+        cv::Mat::zeros(label_height, label_width, CV_32FC(label_channels));
+      for (int y = 0; y < label_height; ++y) {
+        for (int x = 0; x < label_width; ++x) {
+          for (int ch = 0; ch < label_channels; ++ch) {
+            if (crop_label.at<float>(y, x) == ch) {
+              int index = y * label_width * label_channels
+                          + x * label_channels + ch;
+              reinterpret_cast<float *>(multi_ch_label.data)[index] = 1.0;
+            }
           }
         }
       }
-    }
 
-    // revert to Dtype vec
-    ConvertFromCVMat(crop_data,
-                     top_data + this->prefetch_data_.offset(item_id));
-    ConvertFromCVMat(multi_ch_label,
-                     top_label + this->prefetch_label_.offset(item_id));
+      // revert to Dtype vec
+      ConvertFromCVMat(crop_data,
+                       top_data + this->prefetch_data_.offset(item_id));
+      ConvertFromCVMat(multi_ch_label,
+                       top_label + this->prefetch_label_.offset(item_id));
+      break;
+    }
 
     // go to the next iter
     cursor_->Next();
