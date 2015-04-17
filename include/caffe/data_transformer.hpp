31e66f91c53795d537db10079bd8ef1ef0fc900d
Shunta Saito <shunta.saito@gmail.com>
2015-04-18 02:41:25 +0900

    add some layers

--- include/caffe/data_transformer.hpp
+++ include/caffe/data_transformer.hpp
@@ -109,6 +109,39 @@ class DataTransformer {
   vector<Dtype> mean_values_;
 };
 
+/**
+ * @brief Perform some transformation to input data and label.
+ */
+template <typename Dtype>
+class PatchTransformerLayer : public Layer<Dtype> {
+public:
+  explicit PatchTransformerLayer(const LayerParameter &param)
+    : Layer<Dtype>(param) {}
+  void LayerSetUp(const vector<Blob<Dtype>*> &bottom,
+                  const vector<Blob<Dtype>*> &top);
+  virtual void Reshape(const vector<Blob<Dtype>*> &bottom,
+                       const vector<Blob<Dtype>*> &top);
+  virtual inline const char* type() const { return "PatchTransformer"; }
+  virtual inline int MinBottomBlobs() const { return 1; }
+  virtual inline int MaxBottomBlobs() const { return 2; }
+  virtual inline int MinTopBlobs() const { return 1; }
+  virtual inline int MaxTopBlobs() const { return 2; }
+
+protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*> &bottom,
+                           const vector<Blob<Dtype>*> &top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*> &top,
+                            const vector<bool> &propagate_down,
+                            const vector<Blob<Dtype>*> &bottom) {
+    NOT_IMPLEMENTED;
+  }
+
+private:
+  cv::Mat ConvertToCVMat(const Dtype *data, const int &channels,
+                         const int &height, const int &width);
+  void ConvertFromCVMat(const cv::Mat img, Dtype *data);
+};
+
 }  // namespace caffe
 
 #endif  // CAFFE_DATA_TRANSFORMER_HPP_
--- include/caffe/loss_layers.hpp
+++ include/caffe/loss_layers.hpp
@@ -559,6 +559,42 @@ class MultinomialLogisticLossLayer : public LossLayer<Dtype> {
 };
 
 /**
+ * @brief compute area under the precision recall curve (AUPRC)
+ * This layer needs a softmaxed prediction blob and a label blob as inputs.
+ */
+template <typename Dtype>
+class PrecisionRecallLossLayer : public LossLayer<Dtype> {
+ public:
+  explicit PrecisionRecallLossLayer(const LayerParameter &param)
+    : LossLayer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*> &bottom,
+                          const vector<Blob<Dtype>*> &top);
+  virtual void Reshape(const vector<Blob<Dtype>*> &bottom,
+                       const vector<Blob<Dtype>*> &top);
+
+  virtual inline const char *type() const { return "PrecisionRecallLoss"; }
+
+  virtual inline int ExactNumBottomBlobs() const { return 2; }
+  virtual inline int ExactNumTopBlobs() const { return -1; }
+  virtual inline int MinTopBlobs() const { return 1; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*> &bottom,
+                           const vector<Blob<Dtype>*> &top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*> &bottom,
+                           const vector<Blob<Dtype>*> &top);
+  virtual void Backward_cpu(
+    const vector<Blob<Dtype>*> &top,
+    const vector<bool> &propagate_down, const vector<Blob<Dtype>*> &bottom);
+  virtual void Backward_gpu(
+    const vector<Blob<Dtype>*> &top,
+    const vector<bool> &propagate_down, const vector<Blob<Dtype>*> &bottom);
+
+ public:
+  Blob<Dtype> loss_;
+};
+
+/**
  * @brief Computes the cross-entropy (logistic) loss @f$
  *          E = \frac{-1}{n} \sum\limits_{n=1}^N \left[
  *                  p_n \log \hat{p}_n +
@@ -655,6 +691,46 @@ class SigmoidCrossEntropyLossLayer : public LossLayer<Dtype> {
 
 // Forward declare SoftmaxLayer for use in SoftmaxWithLossLayer.
 template <typename Dtype> class SoftmaxLayer;
+
+template <typename Dtype>
+class SoftmaxCrossEntropyLossLayer : public LossLayer<Dtype> {
+ public:
+  explicit SoftmaxCrossEntropyLossLayer(const LayerParameter &param)
+    : LossLayer<Dtype>(param),
+      softmax_layer_(new SoftmaxLayer<Dtype>(param)) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*> &bottom,
+                          const vector<Blob<Dtype>*> &top);
+  virtual void Reshape(const vector<Blob<Dtype>*> &bottom,
+                       const vector<Blob<Dtype>*> &top);
+  virtual inline const char *type() const { return "SoftmaxCrossEntropyLoss"; }
+
+  virtual inline int ExactNumBottomBlobs() const { return 2; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*> &bottom,
+                           const vector<Blob<Dtype>*> &top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*> &bottom,
+                           const vector<Blob<Dtype>*> &top);
+  virtual void Backward_cpu(
+    const vector<Blob<Dtype>*> &top,
+    const vector<bool> &propagate_down, const vector<Blob<Dtype>*> &bottom);
+  virtual void Backward_gpu(
+    const vector<Blob<Dtype>*> &top,
+    const vector<bool> &propagate_down, const vector<Blob<Dtype>*> &bottom);
+
+ public:
+  /// The internal SoftmaxLayer used to map predictions to a distribution.
+  shared_ptr<SoftmaxLayer<Dtype> > softmax_layer_;
+  /// prob stores the output probability predictions from the SoftmaxLayer.
+  Blob<Dtype> prob_;
+  /// bottom vector holder used in call to the underlying SoftmaxLayer::Forward
+  vector<Blob<Dtype>*> softmax_bottom_vec_;
+  /// top vector holder used in call to the underlying SoftmaxLayer::Forward
+  vector<Blob<Dtype>*> softmax_top_vec_;
+  /// to compute loss using GPU
+  Blob<Dtype> loss_;
+};
 
 /**
  * @brief Computes the multinomial logistic loss for a one-of-many
