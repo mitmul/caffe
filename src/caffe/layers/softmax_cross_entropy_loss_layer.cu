abe2e9b12571575bc1b84239e7b5d7dbcb12a22e
Shunta Saito <shunta.saito@gmail.com>
2015-05-04 04:37:21 +0900

    add rr to softmax~.cu

--- src/caffe/layers/softmax_cross_entropy_loss_layer.cu
+++ src/caffe/layers/softmax_cross_entropy_loss_layer.cu
@@ -9,24 +9,25 @@
 #include "caffe/vision_layers.hpp"
 
 namespace caffe {
-
-template <typename Dtype>
+template<typename Dtype>
 void SoftmaxCrossEntropyLossLayer<Dtype>::Forward_gpu(
-  const vector<Blob<Dtype>*> &bottom, const vector<Blob<Dtype>*> &top) {
+  const vector<Blob<Dtype> *>& bottom, const vector<Blob<Dtype> *>& top) {
   softmax_bottom_vec_[0] = bottom[0];
 
   // input details
-  const int count = bottom[0]->count();
-  const int num = bottom[0]->num();
-  const int dim = bottom[0]->count() / num;
+  const int count       = bottom[0]->count();
+  const int num         = bottom[0]->num();
+  const int dim         = bottom[0]->count() / num;
   const int spatial_dim = bottom[0]->width() * bottom[0]->height();
-  const int channels = bottom[0]->channels();
+  const int channels    = bottom[0]->channels();
 
   // all units in this channel goes to zero
   const int zero_channel =
     this->layer_param_.softmax_cross_entropy_loss_param().zero_channel();
+
   if (zero_channel >= 0) {
     Dtype *data = softmax_bottom_vec_[0]->mutable_cpu_data();
+
     for (int i = 0; i < num; ++i) {
       for (int j = 0; j < spatial_dim; ++j) {
         const int index = i * dim + zero_channel * spatial_dim + j;
@@ -35,19 +36,40 @@ void SoftmaxCrossEntropyLossLayer<Dtype>::Forward_gpu(
     }
   }
 
+  // suppression of overparameterization
+  const int redundancy_reduction =
+    this->layer_param_.softmax_cross_entropy_loss_param().redundancy_reduction();
+
+  if (redundancy_reduction >= 0) {
+    Dtype *data = softmax_bottom_vec_[0]->mutable_cpu_data();
+
+    for (int i = 0; i < num; ++i) {
+      for (int j = 0; j < spatial_dim; ++j) {
+        const Dtype phi = data[i * dim + redundancy_reduction * spatial_dim + j];
+
+        for (int c = 0; c < channels; ++c) {
+          const int index = i * dim + c * spatial_dim + j;
+          data[index] -= phi;
+        }
+      }
+    }
+  }
+
   // The forward pass computes the softmax prob values.
   softmax_layer_->Forward(softmax_bottom_vec_, softmax_top_vec_);
 
   // Stable version of loss computation from input data
-  const Dtype *data = prob_.cpu_data();
+  const Dtype *data  = prob_.cpu_data();
   const Dtype *label = bottom[1]->cpu_data();
-  Dtype loss = 0;
+  Dtype loss         = 0;
 
   // Compute the loss (negative log likelihood)
   const google::protobuf::RepeatedField<float> weights =
     this->layer_param_.softmax_cross_entropy_loss_param().weights();
+
   if (weights.size() > 0) {
     CHECK_EQ(weights.size(), bottom[0]->channels());
+
     for (int i = 0; i < num; ++i) {
       for (int j = 0; j < spatial_dim; ++j) {
         for (int c = 0; c < channels; ++c) {
@@ -73,29 +95,35 @@ void SoftmaxCrossEntropyLossLayer<Dtype>::Forward_gpu(
   top[0]->mutable_cpu_data()[0] = loss / num;
 }
 
-template <typename Dtype>
+template<typename Dtype>
 void SoftmaxCrossEntropyLossLayer<Dtype>::Backward_gpu(
-  const vector<Blob<Dtype>*> &top,
-  const vector<bool> &propagate_down,
-  const vector<Blob<Dtype>*> &bottom) {
+  const vector<Blob<Dtype> *>& top,
+  const vector<bool>         & propagate_down,
+  const vector<Blob<Dtype> *>& bottom) {
   if (propagate_down[1]) {
     LOG(FATAL) << this->type()
                << " Layer cannot backpropagate to label inputs.";
   }
+
   if (propagate_down[0]) {
     // First, compute the diff
     const int count = bottom[0]->count();
-    const int num = bottom[0]->num();
-    const int dim = bottom[0]->count() / num;
-    const int spatial_dim = bottom[0]->width() * bottom[0]->height();
+    const int num   = bottom[0]->num();
+    const int dim   = bottom[0]->count() /
+                      num;
+    const int spatial_dim = bottom[0]->width() *
+                            bottom[0]->height();
     const int channels = bottom[0]->channels();
-    const Dtype *data = prob_.cpu_data();
+    const Dtype *data  = prob_.cpu_data();
     const Dtype *label = bottom[1]->cpu_data();
-    Dtype *diff = bottom[0]->mutable_cpu_diff();
+    Dtype *diff        =
+      bottom[0]->mutable_cpu_diff();
     const google::protobuf::RepeatedField<float> weights =
       this->layer_param_.softmax_cross_entropy_loss_param().weights();
+
     if (weights.size() > 0) {
       CHECK_EQ(weights.size(), bottom[0]->channels());
+
       for (int i = 0; i < num; ++i) {
         for (int j = 0; j < spatial_dim; ++j) {
           for (int c = 0; c < channels; ++c) {
@@ -107,6 +135,7 @@ void SoftmaxCrossEntropyLossLayer<Dtype>::Backward_gpu(
     } else {
       caffe_sub(count, data, label, diff);
     }
+
     // Scale down gradient
     const Dtype loss_weight = top[0]->cpu_diff()[0];
     caffe_scal(count, loss_weight / num, diff);
@@ -114,5 +143,4 @@ void SoftmaxCrossEntropyLossLayer<Dtype>::Backward_gpu(
 }
 
 INSTANTIATE_LAYER_GPU_FUNCS(SoftmaxCrossEntropyLossLayer);
-
-}  // namespace caffe
+} // namespace caffe
